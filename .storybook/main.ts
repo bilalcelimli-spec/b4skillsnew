7e878cf61238fda9baee7bd39e25c31e346890a7
bilalcelimli-spec <bilalcelimli@gmail.com>
2026-05-25 00:07:45 +0300

    fix: remove addon-essentials (not in Storybook v10), add .npmrc legacy-peer-deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,9 +3,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx|js|jsx|mdx)"],
   addons: [
-    "@storybook/addon-essentials",
     "@storybook/addon-a11y",
-    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
